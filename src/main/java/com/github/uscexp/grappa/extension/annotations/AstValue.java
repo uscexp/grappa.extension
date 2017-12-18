47fa6019243254a6d14d37841540d5c51234cb9e
Eisenhauer Andreas, INI-DEV-COR-OSD-OMM (EXT) <Andreas.Eisenhauer@swisscom.com>
2017-12-18 14:46:23 +0100

    javadoc fixes

--- src/main/java/com/github/uscexp/grappa/extension/annotations/AstValue.java
+++ src/main/java/com/github/uscexp/grappa/extension/annotations/AstValue.java
@@ -10,8 +10,8 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * annotation for parser rules. announce that the interpreter should
- * store the parse value to the stack.
+ * annotation for parser rules. announce that the interpreter should store the
+ * parse value to the stack.
  * 
  * @author haui
  *
@@ -20,16 +20,10 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface AstValue {
-	/**
-	 * type of the value.
-	 */
+
 	public Class<?> valueType() default Object.class;
-	/**
-	 * factory class which holds the factory method.
-	 */
+
 	public Class<?> factoryClass() default Object.class;
-	/**
-	 * factory method name to create the type value from a string.
-	 */
+
 	public String factoryMethod() default "";
 }
--- src/main/java/com/github/uscexp/grappa/extension/interpreter/AstInterpreter.java
+++ src/main/java/com/github/uscexp/grappa/extension/interpreter/AstInterpreter.java
@@ -13,22 +13,27 @@ import com.github.uscexp.grappa.extension.nodes.AstTreeNode;
 import com.github.uscexp.grappa.extension.util.AstTreeUtil;
 
 /**
- * The {@link AstInterpreter} interpretes a PEG defined language, based on {@link BaseParser}. It takes an AST (Abstract Syntax Tree)
- * based on the parsing and the annotated parser methods, annotated with {@link AstValue} or {@link AstCommand}. {@link
- * AstValue} writes the parse node value to the interpreter stack. By default the interpreter stores the values as {@link String}, if one
- * wants to change that, one has to define the corresponding parameters of the {@link AstValue} annotation. {@link AstCommand} must be
- * implemented from a developer, which has to create the Ast<Name>TreeNode and implement it. This represents the logic of your language
- * commands. The {@link AstCommand} classes by default are located in the same package as the parser implementation and have the naming
- * convetion of Ast + name of your parser rule (first char upper case) + TreeNode. If one want to change that, one has to define the
- * corresponding parameters of the {@link AstCommand} annotation.
+ * The {@link AstInterpreter} interpretes a PEG defined language, based on
+ * {@link BaseParser}. It takes an AST (Abstract Syntax Tree) based on the
+ * parsing and the annotated parser methods, annotated with {@link AstValue} or
+ * {@link AstCommand}. {@link AstValue} writes the parse node value to the
+ * interpreter stack. By default the interpreter stores the values as
+ * {@link String}, if one wants to change that, one has to define the
+ * corresponding parameters of the {@link AstValue} annotation.
+ * {@link AstCommand} must be implemented from a developer, which has to create
+ * the Ast'Name'TreeNode and implement it. This represents the logic of your
+ * language commands. The {@link AstCommand} classes by default are located in
+ * the same package as the parser implementation and have the naming convetion
+ * of Ast + name of your parser rule (first char upper case) + TreeNode. If one
+ * want to change that, one has to define the corresponding parameters of the
+ * {@link AstCommand} annotation.
  *
- * @author  haui
+ * @author haui
  */
 public class AstInterpreter<V> {
 
 	private AstTreeNode<V> root;
 	private OutputStream astTreeOutputStream = null;
-	
 
 	public AstInterpreter() {
 		this(null);
@@ -40,28 +45,28 @@ public class AstInterpreter<V> {
 	}
 
 	public void interpretBackwardOrder(Class<? extends BaseParser<V>> parserClass, AstTreeNode<V> node, Long id)
-		throws AstInterpreterException {
+			throws AstInterpreterException {
 		interpret(parserClass, node, id, false);
 	}
 
 	public void interpretForewardOrder(Class<? extends BaseParser<V>> parserClass, AstTreeNode<V> node, Long id)
-		throws AstInterpreterException {
+			throws AstInterpreterException {
 		interpret(parserClass, node, id, true);
 	}
 
 	private void interpret(Class<? extends BaseParser<V>> parserClass, AstTreeNode<V> node, Long id, boolean forewardOrder)
-		throws AstInterpreterException {
+			throws AstInterpreterException {
 		try {
 			ProcessStore.getInstance(id);
 
 			root = node;
-			
-			if(root == null) {
+
+			if (root == null) {
 				throw new AstInterpreterException(String.format("Can't build AstTreeNodes for parser class %s",
 						parserClass.getName()));
 			}
-			
-			if(astTreeOutputStream != null) {
+
+			if (astTreeOutputStream != null) {
 				AstTreeUtil.printAstTree(root, astTreeOutputStream);
 			}
 
